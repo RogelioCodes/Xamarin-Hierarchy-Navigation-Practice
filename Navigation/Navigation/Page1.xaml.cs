d7935a639795d4c4a631ce84744ad8e4a19254bc
RogelioCordova <55772023+droozyson@users.noreply.github.com>
2020-02-12 17:04:51 -0800

    hey

---
+++ Navigation/Navigation/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+       public void OnClick(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+
+        public void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
---
+++ Navigation/Navigation/Page3.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page3 : ContentPage
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+        public void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        public void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PopToRootAsync();
+        }
+    }
+}
